1a2393bfb100d36101ac5a25d2d1dbadfb7fa65b
yanyinuo <1601530253@qq.com>
2021-12-09 23:13:23 +0800

    12-9

--- dao/socket.js
+++ dao/socket.js
@@ -1,9 +1,28 @@
 module.exports = function (io) {
+    var users = {}
+
     io.on('connection', (socket) => {
 
         socket.on('login', (id) => {
-            console.log(id)
-            socket.emit('msg', id)
+            socket.name = id
+            users[id] = socket.id
+            socket.emit('msg', socket.id)
+
+        })
+
+        //用户一对一消息发送
+        socket.on('msg', (msg, fromId, toId) => {
+            console.log(msg);
+            socket.emit('msg', msg)
+        })
+
+
+        //用户离开
+        socket.on('disconnecting', () => {
+            if (users.hasOwnProperty(socket.name)) {
+                delete users[socket.name]
+                console.log(socket.id + '离开');
+            }
         })
 
     })
